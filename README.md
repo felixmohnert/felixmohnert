6d816551c4392d79c9eff4819eed1e0138715823
Felix Mohnert <felix.mohnert@absolventa.de>
2013-10-20 00:02:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+== Info to and from felixmohnert
+
+This is my personal portfolio app. If you feel inspired and use it for your own purposes.
+Special thanks to https://github.com/IronSummitMedia for the awesome template.
+
+Build with:
+
+* Ruby 2.0
+
+* Rails 4.0
+
+* Bootstrap 3
+
+* My own blogging implementation
+
+* Postgres
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Felixmohnert::Application.config.secret_key_base = '5a6b00ab3e532be0da2c7a9fc6f4e914cf05412a30f2265dddf74216dd4a3a029c521e87885f737863304e061ea8f920c7c9fc413034f740129b0a5c49e42b89'
