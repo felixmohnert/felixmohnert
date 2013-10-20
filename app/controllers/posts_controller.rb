3c7c15718213df12ef12fdb24403517e1693d7fc
Felix Mohnert <felix.mohnert@absolventa.de>
2013-10-20 19:40:56 +0200

    Modified posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
+
+  before_filter :authenticate, :only => [:new, :create]
+
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: 'Post was successfully created'}
+      else
+        format.html { render action: 'new' }
+      end
+    end
+  end
+
+
+  protected
+
+  def post_params
+    params.require(:post).permit([:title, :content])
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USER'] && password == ENV['PASSWORD']
+    end
+  end
+
 end
